3fa88bbee22e84efe7bd34917d8ea65690bd6967
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-17 14:22:14 +0000

    Add AddButton Component

---
+++ src/AddButton/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class AddButton extends Component{
+  render(){
+    return(
+      <button>+</button>
+    )
+  }
+}
+
+default export AddButton;

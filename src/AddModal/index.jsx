ea6eb4ca3b888eaa954d3922fb42f41cb50d9abf
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-17 23:14:48 +0000

    add a display as a state on AddModal

--- src/AddModal/index.jsx
+++ src/AddModal/index.jsx
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import './index.css';
 
 class AddModal extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      style: {display: 'block'}
+    }
+  }
   render(){
     return(
       <>
         <button className="add-button"><ion-icon name="add"></ion-icon></button>
-        <div className="add-modal">
+        <div className="add-modal" style={this.state.style}>
           <div className="add-modal-content">
             <h1>Welcome</h1>
             <p>Hello blababla it's just a test</p>

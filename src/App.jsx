a374c887c55e13546eab64be8f296fa7a328f52f
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-17 13:07:20 +0000

    Note basic styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav/index';
+import Note from './Note/index';
 
 function App() {
   return (
     <>
       <Nav/>
+      <Note title="tesaja" body="halohalohalo" bgcolor="yellow"/>
     </>
   );
 }
--- src/Note/index.jsx
+++ src/Note/index.jsx
@@ -3,12 +3,13 @@ import './index.css';
 
 class Note extends Component{
   render(){
-    const bgColor = {backgroundColor: this.props.bgcolor};
     return(
-      <div class="note" style={bgColor}>
+      <div className={`note ${this.props.bgcolor}`}>
         <h2>{this.props.title}</h2>
         <p>{this.props.body}</p>
       </div>
     )
   }
 }
+
+export default Note;

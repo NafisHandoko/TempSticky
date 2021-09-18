e1810f79aac2dcbd4ca9641a03c2ba0df360013a
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-18 02:07:54 +0000

    add enableModal and disableModal method on AddModal component

--- src/AddModal/index.jsx
+++ src/AddModal/index.jsx
@@ -5,18 +5,29 @@ class AddModal extends Component{
   constructor(props){
     super(props);
     this.state = {
-      style: {display: 'block'}
+      style: {display: 'none'}
+    }
+    this.enableModal = () => {
+      this.setState({
+        style: {display: 'block'}
+      })
+    }
+    this.disableModal = () => {
+      this.setState({
+        style: {display: 'none'}
+      })
     }
   }
   render(){
     return(
       <>
-        <button className="add-button"><ion-icon name="add"></ion-icon></button>
+        <button className="add-button" onClick={this.enableModal}><ion-icon name="add"></ion-icon></button>
         <div className="add-modal" style={this.state.style}>
           <div className="add-modal-content">
             <h1>Welcome</h1>
             <p>Hello blababla it's just a test</p>
           </div>
+          <button onClick={this.disableModal}>Ok</button>
         </div>
       </>
     )

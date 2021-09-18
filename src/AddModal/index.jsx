6fd4f1cd45f7bcbc90f6d949c2d981b9cc9e129f
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-18 12:20:15 +0000

    change add-modal-box element from div to form and add title-input and note-input to AddModal component

--- src/AddModal/index.jsx
+++ src/AddModal/index.jsx
@@ -23,16 +23,16 @@ class AddModal extends Component{
       <>
         <button className="add-button" onClick={this.enableModal}><ion-icon name="add"></ion-icon></button>
         <div className="add-modal" style={this.state.style}>
-          <div className="add-modal-box">
+          <form className="add-modal-box">
             <div className="add-modal-content">
-              <h1>Welcome</h1>
-              <p>Hello blababla it's just a test</p>
+              <input className="add-modal-title-input" type="text" placeholder="add a title"/>
+              <input className="add-modal-note-input" type="text" placeholder="your notes here"/>
             </div>
             <div className="add-modal-btn-group">
-              <button className="add-modal-cancel-button" onClick={this.disableModal}><ion-icon name="close-sharp"></ion-icon></button>
-              <button className="add-modal-submit-button" onClick={this.disableModal}><ion-icon name="checkmark-sharp"></ion-icon></button>
+              <button type="button" className="add-modal-cancel-button" onClick={this.disableModal}><ion-icon name="close-sharp"></ion-icon></button>
+              <button type="submit" className="add-modal-submit-button"><ion-icon name="checkmark-sharp"></ion-icon></button>
             </div>
-          </div>
+          </form>
         </div>
       </>
     )

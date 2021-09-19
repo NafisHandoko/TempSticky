7e4b8cc9dadc9d90dceb61d4c54bfb3e88cc256c
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-19 12:27:36 +0000

    add state to App Component for Note component data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Nav from './Nav/index';
 import Note from './Note/index';
 import AddModal from './AddModal/index';
 
-function App() {
-  return (
-    <>
-      <Nav/>
-      <div className="body-container">
-        <Note title="tesaja" body="halohalohalo" bgcolor="yellow"/>
-        <Note title="halo dunia" body="ini noteku yang lain" bgcolor="green"/>
-        <AddModal/>
-      </div>
-    </>
-  );
+// function App() {
+//   return (
+//     <>
+//       <Nav/>
+//       <div className="body-container">
+//         <Note title="tesaja" body="halohalohalo" bgcolor="yellow"/>
+//         <Note title="halo dunia" body="ini noteku yang lain" bgcolor="green"/>
+//         <AddModal/>
+//       </div>
+//     </>
+//   );
+// }
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      notedata: [
+        {id:"1", title:"tesaja", body:"halohalohalo", bgcolor:"yellow"},
+        {id:"2", title:"halo dunia", body:"ini noteku yang lain", bgcolor:"green"}
+      ]
+    }
+  }
+  render(){
+    const notedata = this.state.notedata;
+    return(
+      <>
+        <Nav/>
+        <div className="body-container">
+          {notedata.map((data) =>
+            <Note key={data.id} title={data.title} body={data.body} bgcolor={data.bgcolor}/>
+          )}
+          <AddModal/>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;

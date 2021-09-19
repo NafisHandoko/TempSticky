9f49026e7be3182ab03aa67fd8accfb634953b67
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-09-19 04:41:32 +0000

    add radiobutton to AddModal component to change AddModal component background-color

--- src/AddModal/index.jsx
+++ src/AddModal/index.jsx
@@ -5,10 +5,13 @@ class AddModal extends Component{
   constructor(props){
     super(props);
     this.state = {
-      style: {display: 'none'},
       input: {
         title: '',
-        note: ''
+        note: '',
+        notecolor: '#FFEBAE'
+      },
+      style: {
+        display: 'none'
       }
     }
     this.displayModal = () => {
@@ -21,7 +24,7 @@ class AddModal extends Component{
         ...this.state,
         input: {
           ...this.state.input,
-          [event.target.id]: event.target.value
+          [event.target.name]: event.target.value
         }
       })
     }
@@ -31,14 +34,19 @@ class AddModal extends Component{
       <>
         <button className="add-button" onClick={this.displayModal}><ion-icon name="add"></ion-icon></button>
         <div className="add-modal" style={this.state.style}>
-          <form className="add-modal-box">
+          <form className="add-modal-box" style={{backgroundColor: this.state.input.notecolor}}>
             <div className="add-modal-content">
-              <input id="title" className="add-modal-title-input" type="text" placeholder="add a title" value={this.state.input.title} onChange={this.handleChange}/>
-              <textarea id="note" className="add-modal-note-input" placeholder="your notes here" value={this.state.input.note} onChange={this.handleChange}></textarea>
+              <input name="title" className="add-modal-title-input" type="text" placeholder="add a title" value={this.state.input.title} onChange={this.handleChange}/>
+              <textarea name="note" className="add-modal-note-input" placeholder="your notes here" value={this.state.input.note} onChange={this.handleChange}></textarea>
             </div>
             <div className="add-modal-btn-group">
               <button type="button" className="add-modal-cancel-button" onClick={this.displayModal}><ion-icon name="close-sharp"></ion-icon></button>
               <button type="submit" className="add-modal-submit-button"><ion-icon name="checkmark-sharp"></ion-icon></button>
+            </div>
+            <div className="note-color-radio">
+              <input type="radio" name="notecolor" value="#FFEBAE" onClick={this.handleChange}/>
+              <input type="radio" name="notecolor" value="#FFDBDB" onClick={this.handleChange}/>
+              <input type="radio" name="notecolor" value="#D8FFDC" onClick={this.handleChange}/>
             </div>
           </form>
         </div>
